45c087c096fddd8e46930bf797099c114b0a4469
bigsan33 <yuichi887+sub@gmail.com>
2019-05-06 01:47:19 +0900

    PinBallプロジェクト
    Lesson5課題提出です。

---
+++ Assets/BallController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BallController : MonoBehaviour {
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+
+    //スコア合計
+    private int scoreSum = 0;
+
+
+    // Use this for initialization
+    void Start() {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+
+        //シーン中のScoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ) {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+
+        //スコアを表示
+        Text score = this.scoreText.GetComponent<Text>();
+        score.text = "Score:" + scoreSum;
+    }
+
+    //衝突時に呼ばれる関数
+    private void OnCollisionEnter(Collision other) {
+        //Debug.Log(other.gameObject.tag);
+
+        //SmallStarTagは10点
+        if (other.gameObject.tag == "SmallStarTag") {
+            scoreSum += 10;
+        }
+
+        //SmallCloudTagは20点
+        if (other.gameObject.tag == "SmallCloudTag") {
+            scoreSum += 20;
+        }
+
+        //LargeStarTagは50点
+        if (other.gameObject.tag == "LargeStarTag") {
+            scoreSum += 50;
+        }
+
+        //LargeCloudTagは100点
+        if (other.gameObject.tag == "LargeCloudTag") {
+            scoreSum += 100;
+        }
+    }
+}
---
+++ Assets/FlipperComtroller.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FlipperComtroller : MonoBehaviour {
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        //HingJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+
+        //Debug.Log("Screen Width : " + Screen.width);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //左矢印キーを押した時左フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+            SetAngle(this.flickAngle);
+        }
+
+        //右矢印キーを押した時右フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag") {
+            SetAngle(this.flickAngle);
+        }
+
+        //矢印キー離された時フリッパーを元に戻す
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+            SetAngle(this.defaultAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag") {
+            SetAngle(this.defaultAngle);
+        }
+
+        //タッチ操作
+        for (int i = 0; i < Input.touchCount; i++) {
+            Touch touch = Input.GetTouch(i);
+
+            //タッチ開始
+            if (touch.phase == TouchPhase.Began) {
+
+                //左矢印キーを押した時左フリッパーを動かす
+                if (touch.position.x <= Screen.width / 2 && tag == "LeftFripperTag") {
+                    SetAngle(this.flickAngle);
+                    //Vector2 pos = touch.position;
+
+                    //float x = Input.GetTouch(i).position.x;
+                    //Debug.Log(x);
+                    //Debug.Log(tag);
+                }
+                //右矢印キーを押した時右フリッパーを動かす
+                if (touch.position.x > Screen.width / 2 && tag == "RightFripperTag") {
+                    SetAngle(this.flickAngle);
+
+                    //float x = Input.GetTouch(i).position.x;
+                    //Debug.Log(x);
+                    //Debug.Log(tag);
+                }
+            }
+
+            //タッチ終了
+            if (touch.phase == TouchPhase.Ended) {
+
+                //矢印キー離された時フリッパーを元に戻す
+                if (touch.position.x <= Screen.width / 2 && tag == "LeftFripperTag") {
+                    SetAngle(this.defaultAngle);
+                }
+                if (touch.position.x > Screen.width / 2 && tag == "RightFripperTag") {
+                    SetAngle(this.defaultAngle);
+                }
+            }
+        }
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle) {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+ }
